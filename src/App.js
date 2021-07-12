0b03498fe3630971b699864f4e3ba5c3c85103e0
minsoo kim <zlemzlem5656@naver.com>
2021-07-13 00:20:51 +0900

    [추가] 로그인 페이지 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/home">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
