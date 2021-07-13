af0c3f032b85eba34d4ae10be3ab14e6aa66682c
minsoo kim <zlemzlem5656@naver.com>
2021-07-13 18:17:55 +0900

    [추가] rightSide 퍼블리싱

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -4,7 +4,32 @@ import styled from "styled-components";
 const RightSide = () => {
   return (
     <Container>
-      <FollowCard>rightside</FollowCard>
+      <FollowCard>
+        <Title>
+          <h2>Add to your feed</h2>
+          <img src="/images/feed-icon.svg" alt="feed-icon" />
+        </Title>
+        <FeedList>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Linkedin</span>
+              <button>Follow</button>
+            </div>
+          </li>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Video</span>
+              <button>Follow</button>
+            </div>
+          </li>
+        </FeedList>
+      </FollowCard>
     </Container>
   );
 };
@@ -25,3 +50,20 @@ const FollowCard = styled.div`
   box-shadow: 0 0 0 1px rgba(0 0 0 / 15%), 0 0 0 rgba(0 0 0 / 20%);
   padding: 12px;
 `;
+
+const Title = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  width: 100%;
+  color: rgba(0, 0, 0, 0.6);
+  h2 {
+  }
+  img {
+  }
+`;
+
+const FeedList = styled.ul``;
+
+const Avatar = styled.div``;
